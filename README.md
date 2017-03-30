8de673fcea5d251e24d2c0226e700d490196d939
Maria Camila Remolina Gutierrez <mariacamilaremolinagutierrez@gmail.com>
2017-03-30 08:02:36 -0500

    code review

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Para correr el proyecto:
 1. meteor npm install
 
 2. MONGO_URL=mongodb://heroku_cl5chmxx:hob4t7pao4kqp7gmjrgppk4pj@ds113680.mlab.com:13680/heroku_cl5chmxx meteor
+
+
+#Comentarios del code review
+En general me gusta mucho la pagina, en particular y respecto al P3 tiene un diseño
+muy lindo. Me agrada que esta pensada para un publico local y espero que mucha gente
+la use :) 
--- imports/startup/accounts-config.js
+++ imports/startup/accounts-config.js
@@ -1,12 +1,14 @@
 import { Accounts } from 'meteor/accounts-base';
 import { Meteor } from 'meteor/meteor';
 
+//Super que usen accounts.ui, facilita mucho las cosas :)
 Accounts.ui.config({
   passwordSignupFields: 'USERNAME_ONLY',
 });
 
 if (Meteor.isServer) {
   Accounts.onCreateUser(function (options, user) {
+    // Me gusta mucho que manejen roles, pensando desde ya en la seguridad ;) 
     user.role = 'user';
     // Se mantiene el comportamiento por defecto de login.
     if (options.profile)
