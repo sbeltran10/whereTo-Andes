96d7af8367fbc812a1ec8f1205a56ac63c5f5e52
Sergio Velásquez <sy.velasquez10@uniandes.edu.co>
2017-04-16 14:47:05 -0500

    Cambio de click en ir a Preguntas

--- imports/ui/components/Header.jsx
+++ imports/ui/components/Header.jsx
@@ -3,6 +3,19 @@ import AccountsUIWrapper from '../AccountsUIWrapper.jsx';
 
 class Header extends Component {
 
+
+  irPreguntas(){
+
+    document.getElementById('promo').style.display = 'none'
+    setTimeout(function (){
+        document.getElementById('promo').style.display = 'block';
+      $('html,body').animate({
+        scrollTop: $("#preguntas").offset().top
+      },
+        'slow');}
+    , 1000);
+
+  }
   render() {
     return(
       <div>
@@ -30,7 +43,7 @@ class Header extends Component {
                 <h2 className="title">Where<span className="highlight">-To</span></h2>
                 <p className="intro">Conoce los lugares, espacios o personas en la universidad que te pueden ayudar. Hazlo respondiendo unas sencillas preguntas</p>
                 <div className="btns">
-                    <a className="btn btn-cta-primary" href="#preguntas">Ir a las preguntas</a>
+                    <a className="btn btn-cta-primary" href="#preguntas" onClick={this.irPreguntas.bind(this)}>Ir a las preguntas</a>
                     <a className="nuevo"><AccountsUIWrapper/></a>
                 </div>
                 <br/>
